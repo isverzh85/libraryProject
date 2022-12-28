b6a6c08d2fde4382b852ac47ece4901f746f3e0f
Irina <irinasverzhanovskaya69@gmail.com>
2022-12-27 19:03:04 -0500

    button onclick needs work

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -17,34 +17,23 @@ export const Button = ({
   const fetchData = async () => {
     const subjectsAPI = `https://openlibrary.org/subjects/${genre}.json`;
     const subjectAPIResponse = await Axios.get(subjectsAPI)
-    setSubjects(subjectAPIResponse);
+    setSubjects(subjectAPIResponse.data.works);
+    console.log(subjectAPIResponse)
   }
 
   useEffect(() => {
-    fetchData(`https://openlibrary.org/subjects/${genre}.json`).then(res);
+     fetchData(`https://openlibrary.org/subjects/${genre}.json`).then(res)
   }, []);
 
   return (
     <div className={style.container}>
-        <button className={className} onClick={fetchData} type={type}>{children}</button> 
-
       <div className={style.genreSubject}>
-        {subjects && subjects.map((genreSubject) => {
-           return subjects.map((index) => {
-             return (
-                 <ul className="genre">
-                   <li><Button key={index} genre={genre.data.cooking} onClick={subjects}>{children}</Button></li>
-                    <li><Button key={index} genre={genre.horror} onClick={fetchData}>{children}</Button></li>
-                   <li><Button key={index} genre={genre.fantasy} onClick={fetchData}>{children}</Button></li>
-                   <li><Button key={index} genre={genre.mystery} onClick={fetchData}>{children}</Button></li>
-                   <li><Button key={index} genre={genre.personaldevelopment} onClick={fetchData}>{children}</Button></li>
-                   <li><Button key={index} genre={genre.romance} onClick={fetchData}>{children}</Button></li>
-                   <li><Button key={index} genre={genre.sciFi} onClick={fetchData}>{children}</Button></li> 
-                 </ul>           
-               )
-             })
-           })}
-        </div>
-     </div>
-  )}
+        {subjects && subjects.map(() => {
+           return (
+              <button className={className} onClick={() => setSubjects.data} type={type}>{children}</button>                             
+            )
+          })}
+      </div>
+  </div>
+)}
 export default Button 
