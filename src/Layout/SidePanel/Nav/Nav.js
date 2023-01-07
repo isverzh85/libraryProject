a74d610a35e5d3a947db349f466085cf78f6884f
Irina <irinasverzhanovskaya69@gmail.com>
2023-01-07 15:14:36 -0500

    book list showing

--- src/Layout/SidePanel/Nav/Nav.js
+++ src/Layout/SidePanel/Nav/Nav.js
@@ -1,41 +1,37 @@
 import React from "react";
 import styles from '../../SidePanel/Nav/styles.module.scss';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Axios from 'axios';
 
-export const Nav = ({
-  genre,
-  
-}) => {
+export const Nav = () => {
 
-  const [bookLists, setBookList] = useState([]);
-  
-  const getBookData = async () => {
+  const [bookLists, setBookList] = useState([]); 
+  const getBookData = async (genre) => {
     const bookListAPI = `https://openlibrary.org/subjects/${genre}.json`;
     const bookListAPIResponse = await Axios.get(bookListAPI)
-    setBookList(bookListAPIResponse.data);
-    console.log(bookListAPIResponse)
+    setBookList(bookListAPIResponse.data.works);
   };
 
  return (
       <div className={styles.list}>
-         {bookLists.length > 0 && bookLists.map((bookList => {
-          return(
-            <button type="button" onClick={() => setBookList(bookLists)}>cooking</button> 
-            <button type="button" onClick={() => setBookList(bookLists)}>horror</button>
-            <button className={styles.navListItem3} type="button">fantasy</button>
-            <button className={styles.navListItem4} type="button">mystery</button>
-            <button className={styles.navListItem5} type="button">personal development</button>
-            <button className={styles.navListItem6} type="button">romance</button>
-            <button className={styles.navListItem7} type="button">sci-fi</button>
-          </div>
-         )
-      }))}
-    )
-
-       
-     
-    
-
-
+         {bookLists.length > 0 && bookLists.map((book => {
+            return (
+             <div>             
+                {book.title}
+             </div>
+              )
+            })
+        )}
+      <div>
+          <button type="button" className={styles.navListItem1} onClick={() => getBookData('cooking')}>cooking</button> 
+          <button type="button" className={styles.navListItem2} onClick={() => getBookData('horror')}>horror</button>
+          <button type="button" className={styles.navListItem3} onClick={() => getBookData('fantasy')}>fantasy</button>
+          <button type="button" className={styles.navListItem4} onClick={() => getBookData('mystery')}>mystery</button>
+          <button type="button" className={styles.navListItem5} onClick={() => getBookData('personal development')}>personal development</button>
+          <button type="button" className={styles.navListItem6} onClick={() => getBookData('romance')}>romance</button>
+          <button type="button" className={styles.navListItem7} onClick={() => getBookData('sci-fi')}>sci-fi</button>
+       </div>
+    </div>
+ )};
+ 
 export default Nav;
