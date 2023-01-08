6017f9a2b7b05db765ba3477c29ca79af84bda6b
Irina <irinasverzhanovskaya69@gmail.com>
2023-01-07 20:01:44 -0500

    work in progress

--- src/Layout/SidePanel/Nav/Nav.js
+++ src/Layout/SidePanel/Nav/Nav.js
@@ -6,10 +6,25 @@ import Axios from 'axios';
 export const Nav = () => {
 
   const [bookLists, setBookList] = useState([]); 
+  const [authors, setAuthors] = useState([]);
+  const [covers, setCovers] = useState([]);
+
   const getBookData = async (genre) => {
     const bookListAPI = `https://openlibrary.org/subjects/${genre}.json`;
     const bookListAPIResponse = await Axios.get(bookListAPI)
     setBookList(bookListAPIResponse.data.works);
+  };
+
+  const getAuthorData = async (q) => {
+    const authorsAPI = `https://openlibrary.org/search/authors/json/${q}`;
+    const authorsAPIResponse = await Axios.get(authorsAPI)
+    setAuthors(authorsAPIResponse)
+  };
+
+  const getCoverData = async() => {
+    const coverAPI = `https://covers.openlibrary.org/b/$key/$value-$size.jpg`;
+    const coverAPIResponse = await Axios.get(coverAPI)
+    setCovers(coverAPIResponse)
   };
 
  return (
