eeea47274ae4919c856da84ef97005bfe1f46d86
Irina <irinasverzhanovskaya69@gmail.com>
2023-01-11 11:32:39 -0500

    updates needed for the cover

--- src/Layout/SidePanel/Nav/Nav.js
+++ src/Layout/SidePanel/Nav/Nav.js
@@ -6,36 +6,48 @@ import Axios from 'axios';
 export const Nav = () => {
 
   const [bookLists, setBookList] = useState([]); 
-  const [authors, setAuthors] = useState([]);
-  const [covers, setCovers] = useState([]);
 
   const getBookData = async (genre) => {
+
     const bookListAPI = `https://openlibrary.org/subjects/${genre}.json`;
     const bookListAPIResponse = await Axios.get(bookListAPI)
-    setBookList(bookListAPIResponse.data.works);
-  };
+    setBookList(bookListAPIResponse.data.works)
+  }
 
-  const getAuthorData = async (q) => {
-    const authorsAPI = `https://openlibrary.org/search/authors/json/${q}`;
-    const authorsAPIResponse = await Axios.get(authorsAPI)
-    setAuthors(authorsAPIResponse)
-  };
+  const getCoverAPI = async(key, size, url) => {
+    const coversAPI = `https://covers.openlibrary.org/b/id/${key}-${size}.jpg`;
+    return coversAPI
+  }
 
-  const getCoverData = async() => {
-    const coverAPI = `https://covers.openlibrary.org/b/$key/$value-$size.jpg`;
-    const coverAPIResponse = await Axios.get(coverAPI)
-    setCovers(coverAPIResponse)
-  };
+  async function fetchImage(url) {
+    const img = new Image();
+    return new Promise((res, rej) => {
+        img.onload = () => res(img);
+        img.onerror = e => rej(e);
+        img.src = url;
+    });
+}
+
 
  return (
       <div className={styles.list}>
          {bookLists.length > 0 && bookLists.map((book => {
-            return (
+           return (
+
              <div>             
                 {book.title}
+                {book.authors.map(author => {
+                   return(
+                      <div>{author.name}</div>
+                   )     
+                })
+              } 
+                {() => getCoverAPI(book.cover_edition_key)} 
+                <img src = {getCoverAPI(book.cover_edition_key, 'L')} /> 
+                
              </div>
-              )
-            })
+            )
+           })
         )}
       <div>
           <button type="button" className={styles.navListItem1} onClick={() => getBookData('cooking')}>cooking</button> 
