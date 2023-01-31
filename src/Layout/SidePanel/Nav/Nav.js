81d963ea7dbdbb8bac7008c33cecd6ceb045163f
Irina <irinasverzhanovskaya69@gmail.com>
2023-01-30 19:52:02 -0500

    css in progress

--- src/Layout/SidePanel/Nav/Nav.js
+++ src/Layout/SidePanel/Nav/Nav.js
@@ -14,33 +14,41 @@ export const Nav = () => {
         bookList.forEach((book) => { 
     let bookToRender = {
         title:book.title, 
-        author:book.authors, 
+        authors:book.authors, 
         cover_url: `https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg`,
         first_publish_year: book.first_publish_year
       }
         books.push(bookToRender)
         books.sort((book1, book2) => book2.first_publish_year - book1.first_publish_year);
+        //put logic for the if and else for the year 
         console.log(book)
     })     
         setBookList(books)
    }
-
-
-   
+console.log(bookLists)
  return (
+    
       <div className={styles.list}>
          {bookLists?.length > 0 && bookLists?.map((book => {
+           const year = book.first_publish_year
            return (
-             <div>             
-                {book.title}
-                {book.authors?.map(author => {
-                   return(
-                      <div>{author.name}</div>
+             <div className={styles.bookList}>
+                <div className={styles.year}>{year}</div>
+                <div>
+                  <img className={styles.cover} src={book.cover_url} alt="book cover"  />  
+                </div>
+                  <div className={styles.title}>
+                     {book.title}
+                   </div>
+                    
+                       {book.authors?.map(author => {
+                         return(
+                       <div className={styles.author}>{author.name}</div>
                    )     
                 })
               } 
-                <img src={book.cover_url} alt="book cover"  />  
-             </div>
+              </div>
+             
             )
            })
         )}
@@ -49,7 +57,7 @@ export const Nav = () => {
           <button type="button" className={styles.navListItem2} onClick={() => getBookData('horror')}>horror</button>
           <button type="button" className={styles.navListItem3} onClick={() => getBookData('fantasy')}>fantasy</button>
           <button type="button" className={styles.navListItem4} onClick={() => getBookData('mystery')}>mystery</button>
-          <button type="button" className={styles.navListItem5} onClick={() => getBookData('personal development')}>personal development</button>
+          <button type="button" className={styles.navListItem5} onClick={() => getBookData('personal_development')}>personal development</button>
           <button type="button" className={styles.navListItem6} onClick={() => getBookData('romance')}>romance</button>
           <button type="button" className={styles.navListItem7} onClick={() => getBookData('sci-fi')}>sci-fi</button>
        </div>
