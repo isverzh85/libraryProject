93d457427bacb183c496c683bcfd5d16ec62ef2a
Irina <irinasverzhanovskaya69@gmail.com>
2023-02-09 10:51:03 -0500

    css updates

--- src/Layout/SidePanel/Nav/Nav.js
+++ src/Layout/SidePanel/Nav/Nav.js
@@ -4,27 +4,38 @@ import { useState } from 'react';
 import Axios, { all } from 'axios';
 import cn from 'classnames'
 
+const groupBy = (list, key) => {
+   return list.reduce((result, item) => {
+     (result[item[key]] = result[item[key]] || []).push(item);
+     return result;
+   }, {});
+ };
+
 export const Nav = () => {
     const [bookLists, setBookList] = useState([]); 
-    const getBookData = async (genre) => {
 
+    const getBookData = async (genre) => {
     let books = [];
     const bookListAPI = `https://openlibrary.org/subjects/${genre}.json`;
     const bookListAPIResponse = await Axios.get(bookListAPI)
     let bookList = bookListAPIResponse.data.works;
-        bookList.forEach((book) => { 
+
+    bookList.forEach((book) => { 
     let bookToRender = {
         title:book.title, 
         authors:book.authors, 
         cover_url: `https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg`,
-        first_publish_year: book.first_publish_year
+        first_publish_year: book.first_publish_year,
+        cover_id: book.cover_id
       }
         books.push(bookToRender)
-   })
-      let uniqueBookYears = [...new Set(books.map(book => book.first_publish_year)) ]
-      books = books.filter(book => uniqueBookYears.includes(book.first_publish_year))
-      books.sort((book1, book2) => book2.first_publish_year - book1.first_publish_year)
-      setBookList(books)
+   });
+
+   let groupedBooks = groupBy(books, 'first_publish_year');
+   let years = Object.keys(groupedBooks).sort((year1, year2) => year2 - year1);
+   let bookData = years.map(year => ({ year, books: groupedBooks[year] }));
+
+      setBookList(bookData)
    }
 
  return (
@@ -39,36 +50,42 @@ export const Nav = () => {
                <button type="button" className={cn(styles.navButton, styles.navListItem7)} onClick={() => getBookData('sci-fi')}>sci-fi</button>
                <div className={styles.text}>uses OpenLibrary API</div>
             </nav>
-            <div className={styles.listBook}>  
-              {bookLists?.length > 0 && bookLists?.map((book, index) => {
-               const year = book.first_publish_year
-               let prevYear;
-                  if (index > 0) {
-                  prevYear = bookLists[index - 1].first_publish_year;
-               }
-           return (
-             <div className={styles.yearContainer}>
-                {prevYear !== year && (
-                   <div className={styles.year}>{year}</div>
-                ) 
-               }
-               <div className={styles.bookContainer}>
-               <div className={styles.book}>
-                  <img className={styles.cover} src={book.cover_url} alt="book cover"  />  
-               </div>
-                  <div className={styles.title}>{book.title} </div>
-                       {book.authors?.map(author => {
-                         return(
-                       <div className={styles.author}>{author.name}</div>
-                   )     
-                })
-              } 
+
+            <div className={styles.listBook}>
+                {bookLists?.length > 0 &&
+                bookLists?.map((book, index) => {
+
+            return (
+              <div className={styles.mainContainer}>
+                  <h2 className={styles.year}>{book.year}</h2>
+              <div className={styles.listOfBooks}>
+                   {book.books?.map((book, index) => {
+                  return (
+                    <div className={styles.bookContainer}>
+                      <div className={styles.book}>
+                        {book.cover_id? (
+                            <img
+                               className={styles.cover}
+                               src={book.cover_url}
+                               alt="book cover"
+                            /> 
+                        ) :  <div className={styles.bookCoverContainer}></div>} 
+                      </div>
+                      <div className={styles.title}>{book.title} </div>
+                      {book.authors?.map((author) => {
+                        return (
+                          <div className={styles.author}>{author.name}</div>
+                        );
+                      })}
+                    </div>
+                  );
+                })}
+                </div>
               </div>
-              </div>
-           )
-           })
-         }
-        </div>
+            );
+          })}
+      </div>
+           ) 
     </div>
  )};
  
