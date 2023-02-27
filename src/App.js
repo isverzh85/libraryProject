19734ce8824fb7e9266876f25d185420d1a81418
Irina <irinasverzhanovskaya69@gmail.com>
2023-02-27 14:09:58 -0500

    css in progress

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header className="App-header">
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100&display=swap" rel="stylesheet"/>
-         <Routing/>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" rel="stylesheet"></link>
+                 <Routing/>
       </header>
     </div>
   );
