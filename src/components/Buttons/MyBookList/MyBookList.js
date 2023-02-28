302ac9c3cfaec0e8d0521114ec25c45f20785d03
Irina <irinasverzhanovskaya69@gmail.com>
2023-02-28 18:58:50 -0500

    css updates

--- src/components/Buttons/MyBookList/MyBookList.js
+++ src/components/Buttons/MyBookList/MyBookList.js
@@ -1,49 +1,18 @@
 import React, { useState } from 'react';
 import styles from '../MyBookList/styles.module.scss';
-import Axios from 'axios';
 
-// const MyBookList = ({ books }) => {
-//   return (
-//     <ul>
-//       {books.map((book, index) => (
-//         <li key={index}>
-//           <div>{book.title}</div>
-//           <div>{book.year}</div>
-//           <img src={book.cover_url} alt="Book cover" />
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
 
-const TheBookList = () => {
-//     const [book, setBook] = useState([]);
-
-//   const [bookList, setBookList] = useState([]);
-
-//   const updateBookData = async (title, cover_url, year, genre) => {
-//     const bookListAPI = `https://openlibrary.org/subjects/${genre}.json`;
-//     const bookListAPIResponse = await Axios.get(bookListAPI);
-//     let bookList = bookListAPIResponse.data.works;
-//     let book = {
-//       title: title,
-//       cover_url: cover_url,
-//       year: year
-//     };
-
-//     setBookList([...bookList, book]);
- // };
+export const TheBookList = () => {
 
   return (
       <div className={styles.mainContainer}>
-      <div className={styles.myBookListContainer}>
         <h1 className={styles.bookContainer}>My Book List</h1>
+      <div className={styles.myBookListContainer}>
         <button className={styles.importButton}>Export to CSV</button>
-        {/* <MyBookList books={bookList} /> */}
       </div> 
     </div>
   );
-};
+  }
 
 export default TheBookList;
 
