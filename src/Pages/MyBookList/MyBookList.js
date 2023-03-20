8e36d20f066e44a533065e0e24c590afa43c3731
Irina <irinasverzhanovskaya69@gmail.com>
2023-03-19 21:31:54 -0400

    the form replicating

--- src/Pages/MyBookList/MyBookList.js
+++ src/Pages/MyBookList/MyBookList.js
@@ -1,9 +1,9 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import styles from "../MyBookList/styles.module.scss";
 import { MyAddedBookListContext } from '../../context/BookList/MyBookListContext';
 
 export const MyBookList = () => {
-
+  const [notes, setNotes] = useState({});
   const { myAddedBookList, changeAddedBookList } = useContext(MyAddedBookListContext);
 
   function handleBookListChanges(book) {
@@ -12,7 +12,7 @@ export const MyBookList = () => {
     authors.forEach((author, index) => {
       authorNames += author.name;
       if (index !== authors.length - 1) {
-        authorNames += ', ';
+        authorNames += ',';
       }
     });
     const updatedBookList = [...myAddedBookList, { 
@@ -20,40 +20,66 @@ export const MyBookList = () => {
       authors, 
       cover_url, 
       year: first_publish_year, 
-      cover_id 
+      cover_id, 
+      notes: notes[cover_id] 
     }];
     changeAddedBookList(updatedBookList);
     console.log(`${title} ${authorNames} ${cover_id}`);
   }
 
-  function handleDeleteFromBookList(bookId) {
-    const updatedBookList = myAddedBookList.filter((book) => book.id !== bookId);
+  function handleDeleteFromBookList(coverId) {
+    const removedBook = myAddedBookList.find((book) => book.cover_id === coverId);
+    const updatedBookList = myAddedBookList.filter((book) => book.cover_id !== coverId);
     changeAddedBookList(updatedBookList);
+    console.log(removedBook);
+  }
+
+  function handleNotesSubmission(coverId, event) {
+    const updatedNotes = { ...notes, [coverId]: event.target.value };
+    setNotes(updatedNotes);
+    handleBookListChanges(myAddedBookList.find((book) => book.cover_id === coverId));
   }
 
   return (
     <div className={styles.bookContainer}>
     <div className={styles.bookListContainer}>
-    <div clasName={styles.myBookListContainer}>My book list</div>  
+    <div className={styles.myBookListContainer}>My book list</div>  
     <button className={styles.exportButton}>Export to CSV</button>
     </div>
     <div className={styles.bookInfoContainer}>
-        {myAddedBookList?.map((book) => (
-          <div className={styles.bookDetailsContainer} key={book.id}>
-            <div className={styles.bookTitleContainer}>
-              <img className={styles.image} src={book.cover_url} alt={book.title} />
-              <button  className={styles.deleteButton} onClick={() => handleDeleteFromBookList(book.id)}>-</button>
+        {myAddedBookList?.map((book, index) => (
+             <div className={styles.bookDetailsContainer} key={`${book.cover_id}_${index}`}>
+               <div className={styles.bookImageContainer}>
+                <img className={styles.image} 
+                     src={book.cover_url} 
+                     alt={book.title} 
+                  />
+                </div>
+              <button className={styles.deleteButton} 
+                      onClick={() => handleDeleteFromBookList(book.cover_id)}
+                  >-
+              </button>
+             <div className={styles.bookTitleContainer}>
               <h2 className={styles.title}>{book.title}</h2>
                 {book.authors?.map((author) => (
                 <div className={styles.author} key={author.id}>
                    {author.name}
-              </div>
+                </div>
+
             ))}
                <h2 className={styles.year}>{book.first_publish_year}</h2>
-            </div>
-            <form>
-              <textarea className={styles.form} id="notes" name="Add notes" placeholder="Add notes..."></textarea>
+
+               <form>
+              <textarea 
+                  className={styles.form} 
+                  id={`notes_${book.cover_id}`}
+                  name={`notes_${book.cover_id}`}
+                  placeholder="Add notes..."
+                  value={notes[book.cover_id] || ""}
+                  onChange={(event) => handleNotesSubmission(book.cover_id, event)}
+                ></textarea>
             </form>
+            </div>   
         </div>
        ))} 
     </div>
