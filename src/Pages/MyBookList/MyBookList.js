50549d39772f5b9e6fa9e02fcb49b4044bbe70f7
Irina <irinasverzhanovskaya69@gmail.com>
2023-03-24 10:03:32 -0400

    updates

--- src/Pages/MyBookList/MyBookList.js
+++ src/Pages/MyBookList/MyBookList.js
@@ -1,9 +1,23 @@
 import React, { useState, useContext } from "react";
 import styles from "../MyBookList/styles.module.scss";
+import { useHistory } from "react-router-dom";
 import { CSVLink } from 'react-csv';
+import cn from "classnames";
+import Axios from "axios";
+import logo from "../../../src/assets/logo.png";
 import { MyAddedBookListContext } from '../../context/BookList/MyBookListContext';
 
+const groupBy = (list, key) => {
+  return list.reduce((result, item) => {
+    const year = item[key];
+    (result[year] = result[year] || []).push(item);
+    return result;
+  }, {});
+};
+
 export const MyBookList = () => {
+  const history = useHistory(); 
+  const [bookList, setBookList] = useState([]);
   const { myAddedBookList, changeAddedBookList } = useContext(MyAddedBookListContext);
 
   const csvData = [[
@@ -25,6 +39,28 @@ export const MyBookList = () => {
         book.notes || ""
       ]);
   });
+
+  const getBookData = async (genre) => {
+    let books = [];
+    const bookListAPI = `https://openlibrary.org/subjects/${genre}.json`;
+    const bookListAPIResponse = await Axios.get(bookListAPI);
+    const bookList = bookListAPIResponse.data.works;
+    bookList.forEach((book) => {
+      let bookToRender = {
+         title: book.title,
+         authors: book.authors,
+         cover_url:  `https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg` ,
+         first_publish_year: book.first_publish_year ,
+         cover_id: book.cover_id ,
+       };
+      books.push(bookToRender);
+    });
+
+    let groupedBooks = groupBy(books, "first_publish_year");
+    let years = Object.keys(groupedBooks).sort((year1, year2) => year2 - year1);
+    let bookData = years.map((year) => ({ year, books: groupedBooks[year] }));
+    setBookList(bookData);
+  };
 
   function handleBookListChanges(book) {
     const { title, authors, cover_url, first_publish_year, cover_id } = book;
@@ -67,6 +103,85 @@ export const MyBookList = () => {
   }
 
   return (
+    <div>
+    <div className={styles.navContainer}> 
+        <div className={styles.bookNavigationContainer}>
+          <div className={styles.descriptionContainer}>
+           <div className={styles.descriptionWrapper}>
+             <h1 className={styles.descriptionTitle}>
+                  Simple Book List Maker by Irina S.
+              </h1>
+            <p className={styles.paragraph}>
+               This is a project that displays books based on the genre and when clicked
+               it retrieves the list of books for that genre. It is created using <strong>ReactJS</strong> 
+               and{" "}
+                <strong>OpenLibraryAPI</strong>.
+            </p>
+          </div>
+          <nav className={styles.listButtons}>
+            <button
+              type="button"
+              className={cn(styles.navButton, styles.navListItem1)}
+              onClick={() => getBookData("cooking")}
+            >
+              cooking
+            </button>
+            <button
+              type="button"
+              className={cn(styles.navButton, styles.navListItem2)}
+              onClick={() => getBookData("horror")}
+            >
+              horror
+            </button>
+            <button
+              type="button"
+              className={cn(styles.navButton, styles.navListItem3)}
+              onClick={() => getBookData("fantasy")}
+            >
+              fantasy
+            </button>
+            <button
+              type="button"
+              className={cn(styles.navButton, styles.navListItem4)}
+              onClick={() => getBookData("mystery")}
+            >
+              mystery
+            </button>
+            <button
+              type="button"
+              className={cn(styles.navButton, styles.navListItem5)}
+              onClick={() => getBookData("personal_development")}
+            >
+              personal development
+            </button>
+            <button
+              type="button"
+              className={cn(styles.navButton, styles.navListItem6)}
+              onClick={() => getBookData("romance")}
+            >
+              romance
+            </button>
+            <button
+              type="button"
+              className={cn(styles.navButton, styles.navListItem7)}
+              onClick={() => getBookData("sci-fi")}
+            >
+              sci-fi
+            </button>
+            <div className={styles.separateNav}>
+               <button
+                 type="button"
+                 className={cn(styles.navButton, styles.viewButton)}
+                 onClick={() => { history.push('/') }}
+                >
+                <img src={logo} alt="logo" className={styles.logo} />
+                  Hide my book list
+               </button>
+             </div>
+           </nav>
+    </div> 
+  </div>
+</div>
     <div className={styles.bookContainer}>
         <div className={styles.bookListContainer}>
            <div className={styles.myBookListContainer}>My book list</div>  
@@ -110,6 +225,7 @@ export const MyBookList = () => {
             </div>   
         </div>
        ))} 
+    </div>
     </div>
   </div>
  );
