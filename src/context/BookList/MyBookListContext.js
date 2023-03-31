e90b7c21071185737753ed9ee5824f59bd00d3c1
Irina <irinasverzhanovskaya69@gmail.com>
2023-03-31 11:08:48 -0400

    Updates

--- src/context/BookList/MyBookListContext.js
+++
@@ -1,8 +0,0 @@
-import React, { createContext, useState } from "react";
-
-export const MyAddedBookListContext = createContext({
-  myAddedBookList: "Default Value",
-  changeAddedBookList: (value) => {},
-});
-
-export default MyAddedBookListContext;
