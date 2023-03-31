d0b655293427dc23c24a471a98ad0d4691424d0d
Nichie ♥ <deniseenriquez@ellefsontech.com>
2023-03-31 11:35:13 -0400

    // hashrouter for gh pages?

--- src/routing/index.js
+++ src/routing/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Switch, Route } from 'react-router-dom';
 import MyBookList from '../Pages/MyBookList/MyBookList';
 import HomePage from '../Pages/Home/Home';
 
 function Routes() {
   return (
-    <Router>
+    <HashRouter>
        <div>
           <Switch>
               <Route exact path="/" component={HomePage} />
               <Route path="/my-book-list" component={MyBookList} />
           </Switch>
          </div>
-      </Router>
+      </HashRouter>
    );
 }
 export default Routes;
